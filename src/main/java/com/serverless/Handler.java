610c97d47e1e8d779468784f5442bae7bacd6942
Ajdin Hedzic <ajdinhedzic@Ajdins-MacBook-Pro.local>
2020-04-11 11:27:01 -0500

    initial commit

---
+++ src/main/java/com/serverless/Handler.java
@@ -0,0 +1,28 @@
+package com.serverless;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private static final Logger LOG = LogManager.getLogger(Handler.class);
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		LOG.info("received : " + input);
+
+		Response responseBody = new Response("Current time: " + LocalDateTime.now().toString(), input);
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.build();
+	}
+}
